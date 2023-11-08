aa28643e83a95df1c09d138be4c5ccfd1b506e41
Anas <anas.001falaki@gmail.com>
2023-11-09 00:39:53 +0100

    machi ana

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
+def add_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d}".format(n))
+        return
+    else:
+        i = 1
+        add = 0
+        while i <= n:
+            add += int(argv[i])
+            i += 1
+        print("{:d}".format(add))
+
+
 if __name__ == "__main__":
-    import sys, math
-    result = 0
-    for i in sys.argv:
-        result += int(i)
-        print("{}".format(result))
+    import sys
+    add_arg(sys.argv)
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from variable_load_5 import a
+from variable_load_5 import a
+
+
+def find_var():
     print(a)
+
+
+if __name__ == "__main__":
+    find_var()
