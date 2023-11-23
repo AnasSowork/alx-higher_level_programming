0d5446f345a0b341d8d5dd49f28e43f3eed44729
Anas <anas.001falaki@gmail.com>
2023-11-23 02:26:13 +0100

    aywa

--- 0x06-python_classes_objects/5-square.py
+++ 0x06-python_classes_objects/5-square.py
@@ -17,8 +17,10 @@ class Square:
         """Get/set the current size of the square."""
         return (self.__size)
 
+
     @size.setter
     def size(self, value):
+        """Define a class Square."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
