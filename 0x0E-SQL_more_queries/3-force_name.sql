689b7d78220b04307808d2b0eb28a9b4ece09d80
Anas <anas.001falaki@gmail.com>
2024-01-18 17:33:32 +0100

    if its not queries then what ?

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- a script that creates the table force_name on your MySQL server
+-- And also creates a table in a database
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
