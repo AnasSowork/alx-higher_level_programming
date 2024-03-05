d4f6a4433cfd456d359983b85140cfaa08383563
Anas <anas.001falaki@gmail.com>
2024-03-05 16:28:16 +0100

    curl - Options

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL as an argument and displays all HTTP methods the server will accept
-curl -sI -X OPTIONS "$1" | grep -i allow | awk '{print $2}'
+curl -sI -X OPTIONS "$1" | grep -i Allow | awk -F ': ' '{print $2}'
