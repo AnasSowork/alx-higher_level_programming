7bec08beef4c750e37ec41aecce303c6de27fb7f
Anas <anas.001falaki@gmail.com>
2024-03-05 17:11:42 +0100

    Bash - requests

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a request to a URL passed as an argument and displays only the status code of the response
-curl -s -I "$1" | grep -i "^HTTP" | awk '{print $2}'
+curl -s -o /dev/null -w "%{http_code}" "$1"
