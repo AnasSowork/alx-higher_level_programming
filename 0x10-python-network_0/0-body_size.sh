471659687aa6ae4592b545d2a600bac26e948d1c
Anas <anas.001falaki@gmail.com>
2024-03-05 16:05:07 +0100

    get the size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-URL=$1
-curl -sI $URL | grep -i Content-Length | awk '{print $2}'
+# This script takes a URL as an argument, sends a request to that URL, and displays the size of the body of the response in bytes
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
