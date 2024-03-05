1f56d818b118326957ad02afded2e6f5abbe141a
Anas <anas.001falaki@gmail.com>
2024-03-05 16:49:08 +0100

    python - Find peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Function that finds a peak in a list of unsorted integers.
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    Find a peak in a list of unsorted integers.
+    """
+    if not list_of_integers:
+        return None
+    low, high = 0, len(list_of_integers) - 1
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+    return list_of_integers[low]
