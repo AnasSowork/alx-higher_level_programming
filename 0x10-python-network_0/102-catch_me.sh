b00569c6359dd6cb3c4f9f6e61a6abbf4ebcff0a
Anas <anas.001falaki@gmail.com>
2024-03-05 17:34:20 +0100

    Bash - Catched

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script makes a request to 0.0.0.0:5000/catch_me causing the server to respond with a message containing "You got me!" in the body of the response
-curl -s -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool" >/dev/null 2>&1
+curl -sLX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
