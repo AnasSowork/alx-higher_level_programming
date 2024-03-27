ef13c0a3c545df99bd0e72cb2463f373d216705d
Anas <anas.001falaki@gmail.com>
2024-03-27 18:20:37 +0000

    Write meStatus code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Script that displays the status code of a GET request
-let request = require('request');
+const request = require('request');
 request.get(process.argv[2], function (err, response) {
   if (err) {
     return console.log(err);
