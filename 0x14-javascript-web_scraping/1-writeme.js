29a1f14baa12867fe1928647d5a723dc4488ea22
Anas <anas.001falaki@gmail.com>
2024-03-27 18:15:11 +0000

    Write meStatus code

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,20 +1,8 @@
 #!/usr/bin/node
-
 // Script that writes a string to a file
-
 const fs = require('fs');
-
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-if (!filePath || !content) {
-  console.error('Usage: ./script.js <file_path> <content>');
-  process.exit(1);
-}
-
-fs.writeFile(filePath, content, 'utf8', function (err) {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
   if (err) {
-    console.error(err);
-    return;
+    return console.log(err);
   }
 });
